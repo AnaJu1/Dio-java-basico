5a6cdc70fcf474120c28aea5dcf4a7ee000ecb6f
AnaJu1 <anajuliadeo@gmail.com>
2022-12-02 18:00:24 -0300

    estudos-java

---
+++ Java-classes/java-anatomia-classes/src/ana/julia/terceirasemana/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+import javax.print.DocFlavor.STRING;
+
+public class Operadores {
+    
+    public static void main (String [] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && condicao2){
+            System.out.println("uma das condicoes é verdadeira");
+        }
+
+        System.out.println("fim");
+    }
+}
